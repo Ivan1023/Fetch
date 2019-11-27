318a167c8a0418de1722b8ac31c4702e7d864e52
Ivan Chau <ivan.chau1023@gmail.com>
2019-11-27 17:14:01 -0500

    map function to display article

--- client/src/components/ArticleCard/ArticleCard.js
+++ client/src/components/ArticleCard/ArticleCard.js
@@ -4,13 +4,14 @@ import './Article.scss'
 
 class Card extends React.Component {
     render(){
+        
         return(
             <article className="article">
-                <img className="article__img-container" src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/pyftvif8itjpptgk221k.jpg" alt="Article Cover"></img>
-                <h1>How to Decorate for the Holidays When You Don't Want To</h1>
-                <p>It’s not enough that we have to do all the holiday shopping, go to all the school concerts and pick up Starbucks gift cards for all the overworked teachers in our lives—we have to turn the house into a red and green winter wonderland, too?</p>
-                <span>Lifehacker.com</span>
-                <span>2019-11-25</span>
+                <img className="article__img-container" src={this.props.article.urlToImage} alt="Article Cover"></img>
+                <h1>{this.props.article.title}</h1>
+                <p>{this.props.article.description}</p>
+                <span>{this.props.article.source.name}</span>
+                <span>{this.props.article.publishedAt}</span>
             </article>
         )
     }
--- client/src/pages/MainPage/Main.js
+++ client/src/pages/MainPage/Main.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Card from '../../components/ArticleCard/ArticleCard'
+import jsonData from '../../data/data.json'
 
+// console.log(data.articles)
 
 class Main extends React.Component {
+    state = {
+        articleData: jsonData.articles
+    }
+
+    
+
     render(){
+        // console.log(this.state.newsArticles)
+        
         return(
             <div>
-                <Card/>
+                {this.state.articleData ? this.state.articleData.map(articleArray => (<Card article={articleArray}/>)): null }
             </div>
         )
     }
